426f33d87d9be3a7e66f7b380bfe2d5ab2154231
Pouja Nikray <poujanik@gmail.com>
2015-10-16 15:38:33 +0200

    Changed newmember page regarding member limit

--- scripts/views/Newmember.jsx
+++ scripts/views/Newmember.jsx
@@ -4,10 +4,9 @@ export default React.createClass({
     render(){
         return (<div className="col-md-8 col-md-offset-2 col-xs-12">
             <h1>Joining the association</h1>
-            <p>You can join DSKV IJzersterk by filling out <a href="https://docs.google.com/forms/d/1YzjtQxjKTAcaJh8SCVK1rlBuRQH2FuoV6KrkkDNy6cg/viewform">this Google form</a>. After that we will arrange a meeting with the current board. The board will like to get to know you better and to see where you are currently standing, what you goals are and what we expect from you and what you expect from us. The last step will then be to buy (obligatory when joining IJzersterk) the t-shirt.</p>
-            <p>With the t-shirt in hand and after meeting with the board you will be able train together with us at the VKR and gain access to the Wiki and you will be added to the private FaceBook group and WhatsApp group. Furthermore we also hold the Strongest Student competition each year which can be a great moment to test your strength when you join us!</p>
-
-            <p>Remember: at IJzesterk it is not important where you are, but how hard you are willing to work to achieve your goal(s).</p>
+            <p>Due the fast growth of the association we are currently at our member limit. If you fill out <a href="https://docs.google.com/forms/d/1YzjtQxjKTAcaJh8SCVK1rlBuRQH2FuoV6KrkkDNy6cg/viewform">the google form</a> you will be put in the waiting list. You will be first the first person to be contacted when the member limit increased.</p>
+            <p>
+            You can always ask us in the gym for help! Follow us at <a href="https://www.facebook.com/ijzersterkdelft">Facebook</a> and our <a href="https://www.youtube.com/channel/UCU2lMvyuV272KSnNmr1hVYg">YouTube channel</a> to stay up to date!</p>
         </div>);
     }
 });
